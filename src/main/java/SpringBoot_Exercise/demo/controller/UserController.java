cd9cf4aefae417b79dadb28108839c72fc9548c4
ChoiHyunSan <chsan626@gmail.com>
2024-12-08 23:57:45 +0900

    로그인 구현

--- src/main/java/SpringBoot_Exercise/demo/controller/UserController.java
+++ src/main/java/SpringBoot_Exercise/demo/controller/UserController.java
@@ -4,7 +4,9 @@ import SpringBoot_Exercise.demo.domain.dto.LoginForm;
 import SpringBoot_Exercise.demo.domain.dto.UserDto;
 import SpringBoot_Exercise.demo.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,11 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
     @GetMapping("login")
     public String login(Model model) {
-        model.addAttribute("loginForm", new LoginForm());
+        if (!model.containsAttribute("loginForm")) {
+            model.addAttribute("loginForm", new LoginForm());
+        }
         return "user_login";
     }
 
@@ -29,6 +36,7 @@ public class UserController {
                         Model model) {
         if(bindingResult.hasErrors() || !userService.login(loginForm)) {
             model.addAttribute("loginError", true);
+            model.addAttribute("loginForm", loginForm);
             return "user_login";
         }
         // 만약 로그인에 성공했다면 세션을 만들어야 하지 않을까?
@@ -48,16 +56,17 @@ public class UserController {
         if(bindingResult.hasErrors()){
             return "user_signUp";
         }
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        userService.registerUser(userDto.getName(),
-                passwordEncoder.encode(userDto.getPassword()),
-                userDto.getEmail());
+
+        try {
+            userService.registerUser(userDto.getName(),
+                    passwordEncoder.encode(userDto.getPassword()),
+                    userDto.getEmail());
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "user_signUp";
+        }
+
         return "redirect:/question/list";
     }
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    private final UserService userService;
 }
